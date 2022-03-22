6f9dfac79e5383d56c28c9f8354c959c2250e253
angelestea <angelhidrobo2015@gmail.com>
2022-03-22 08:07:29 -0500

    Fibonacci

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a = 0;
+    int b = 1;
+    int n;
+
+    printf("How many terms of Fibonacci sequence do you want to get?: ");
+    scanf("%i", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+      printf("%i ", a);
+      a += b;
+      b = a - b;
+    }
+
     return 0;
 }
